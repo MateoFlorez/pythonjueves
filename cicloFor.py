dca2c345757b9321924fc167f3c475356d316767
308 <308@academicos.cesde.edu.co>
2022-03-03 07:39:18 -0500

    NTJ-001-logica python- for para recorrer un arreglo,lista en python

---
+++ cicloFor.py
@@ -0,0 +1,10 @@
+#for i in range(1, 100, 12):
+#    print(i)
+
+# Trabajo con ARREGLOS (LISTAS)
+nombres = ["Mateo","Raquel","Tommy"]
+print(nombres)
+
+# for para RECORRER listas en Python
+for nombre in nombres:
+    print(nombre)
